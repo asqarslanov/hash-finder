19489e6b9231ca0a6122977ec0b943bcbaa441ad
Asqar Arslanov <asqarslanov@gmail.com>
2024-12-22 02:50:09 +0300

    perf: parallelize computations

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
+use rayon::prelude::*;
+
 pub fn find(n: usize, f: usize) {
     let zeros = &"0".repeat(n);
 
     (1_u32..)
+        .par_bridge()
         .map(|num| (num, sha256::digest(num.to_string())))
         .filter(|(_, hash)| hash.ends_with(zeros))
-        .take(f)
+        .take_any(f)
         .for_each(|(num, hash)| {
             println!(r#"{num}, "{hash}""#);
         })
