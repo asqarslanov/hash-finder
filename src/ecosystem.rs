929de864db4f09d0ec6d9279518906f0fb552f32
Asqar Arslanov <asqarslanov@gmail.com>
2024-12-27 00:30:00 +0500

    test: test ecosystem version

--- src/ecosystem.rs
+++ src/ecosystem.rs
@@ -8,3 +8,31 @@ pub fn find(zeros: usize) -> impl ParallelIterator<Item = (u32, String)> {
         .map(|num| (num, sha256::digest(num.to_string())))
         .filter(move |(_, hash)| hash.ends_with(&n_zeros))
 }
+
+#[cfg(test)]
+mod tests {
+    use std::ops::RangeInclusive;
+
+    use super::*;
+
+    const TEST_NS: RangeInclusive<usize> = 1..=4;
+    const TEST_F: usize = 25;
+
+    #[test]
+    fn hashes_match() {
+        for zeros in TEST_NS {
+            find(zeros).take_any(TEST_F).for_each(|(num, hash)| {
+                assert_eq!(hash, sha256::digest(num.to_string()));
+            })
+        }
+    }
+
+    #[test]
+    fn zeros_match() {
+        for zeros in TEST_NS {
+            find(zeros)
+                .take_any(TEST_F)
+                .for_each(|(_, hash)| assert!(hash.ends_with(&"0".repeat(zeros))));
+        }
+    }
+}
