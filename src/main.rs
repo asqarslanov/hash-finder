3307a1fdbcf22b89077d085dbb54ec8ed737711e
Asqar Arslanov <asqarslanov@gmail.com>
2024-12-26 12:41:11 +0500

    docs: add comments to main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ fn main() {
     {
         use rayon::iter::ParallelIterator;
 
-        hash_finder::with_crates(args.zeros)
-            .take_any(args.results)
+        hash_finder::with_crates(args.zeros /* N */)
+            .take_any(args.results /* F */)
             .for_each(|(number, hash)| {
                 println!("{}", format(number, &hash));
             });
@@ -20,14 +20,16 @@ fn main() {
 
     #[cfg(not(feature = "ecosystem"))]
     {
-        hash_finder::without_crates(args.zeros)
-            .take(args.results)
+        hash_finder::without_crates(args.zeros /* N */)
+            .take(args.results /* F */)
             .for_each(|(number, hash)| {
                 println!("{}", format(number, &hash));
             });
     }
 }
 
+/// Formats the given number and string such as the following.
+/// - `123, "abc123dfg456"`
 fn format(number: u32, hash: &str) -> String {
     format!(r#"{number}, "{hash}""#)
 }
